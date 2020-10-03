447db1f0a17cf774d3e5c8454eaa4c90d7bc9271
Pjotr <Pjotr@DESKTOP-MD4ICKB>
2020-10-03 22:13:58 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='botlabs.py',
+    version='0.0.1',
+    author='Pjotr',
+    author_email='pjotrLSPDFR@outlook.com',
+    description='botlabs.py is an simple API wrapper for botlabs.org',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=""
+)
